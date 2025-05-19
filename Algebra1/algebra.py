19ca10b737ca57ed5933bf3db86f2c20d2b26671
Immanuel Egboche <immanuelegboche@Immanuels-Mac-mini.local>
2025-05-19 23:45:59 +0100

    first commit

---
+++ Algebra1/algebra.py
@@ -0,0 +1,44 @@
+"""
+a + b - is the sum of a and b
+
+a + a is 2a 
+
+a + a + a is 3a etc 
+
+a -b is difference between a and b 
+
+a xb or a.b is product of a and b
+
+a.a is a^2
+
+a    c      ad + cb
+-  + - =     -----
+b    d         bd
+
+
+a     c      ad - cb
+-  -  - =     -----
+b     d         bd
+
+
+a    c       ac
+-  . - =     ---
+b    d        bd
+
+a     c      a       d       ad
+-  /  - =     --  .  --  =   ----
+b     d       b     c         bc
+
+
+
+"""
+# 1
+x = 127 -59
+
+y = 127 + 59
+
+print(y - x) 
+
+# 2
+x= 5*(8-6)
+print(x)
